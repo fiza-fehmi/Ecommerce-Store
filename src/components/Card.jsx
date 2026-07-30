dfc0b6469852571dacb78d279a05f5e64bf5cdce
fiza-fehmi <fizafehmi@gmail.com>
2026-07-30 04:00:14 -0700

    Update product card border styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ const Card = ({
   price,
 }) => {
   return (
-    <div className="w-[220px] border border-transparent hover:border hover:border-[#5A3FF2] rounded-lg cursor-pointer transition-all duration-300 ease-in group p-2 bg-[#FFFDF9] shadow-[0_4px_18px_rgba(31,32,56,0.10)]">
+    <div className="w-[220px] border border-transparent hover:border hover:border-gray-600/20 rounded-lg cursor-pointer transition-all duration-300 ease-in group p-2 bg-[#FFFDF9] shadow-[0_4px_18px_rgba(31,32,56,0.10)]">
       <div className="relative h-48 items-center rounded-lg justify-center w-full overflow-hidden flex  bg-[#F2EBFB]">
         <img className="h-full w-full transition-all duration-200 ease-in group-hover:scale-110 rounded-lg relative object-contain" src={img} alt={productName} />
         <div className="absolute top-3 left-2 text-[#5A3FF2] bg-white text-sm rounded-lg text-gray-700 border border-gray-600/20 px-2 py-1">{tag}</div>
